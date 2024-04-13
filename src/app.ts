1340ca6fb8ae66e85f02aef3f2d557437bccf11c
Caio Costa <caiopaulinocosta@hotmail.com>
2024-04-13 12:36:33 -0300

    Integrando express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Deu boa!");
+import express from "express";
+import config from "config";
+
+const app = express();
+
+// JSON middleware
+app.use(express.json());
+
+// app port
+const port = config.get<number>("port");
+
+app.listen(3000, async () => {
+  console.log(`Aplicação está funcionando na porta: ${port}`);
+});
